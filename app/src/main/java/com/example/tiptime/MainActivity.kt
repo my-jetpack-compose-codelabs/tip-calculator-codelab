8cfa40d00ed4a28f66e34016d17911143a761534
FunnyValentine <darkbrewx@gmail.com>
2024-08-16 20:55:18 +0900

    2. 添加了一个接受用户输入的 textfield

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -30,6 +31,7 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -68,8 +70,15 @@ fun TipTimeLayout() {
         Text(
             text = stringResource(R.string.calculate_tip),
             modifier = Modifier
-                .padding(bottom = 16.dp, top = 40.dp)
+                .padding(
+                    bottom = 16.dp,
+                    top = 40.dp
+                )
                 .align(alignment = Alignment.Start)
+        )
+        // 添加一个输入框,输入账单的价格
+        EditNumberField(
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
         )
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
@@ -79,6 +88,15 @@ fun TipTimeLayout() {
     }
 }
 
+// 自定义一个输入框组件
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        modifier = modifier
+    )
+}
 /**
  * Calculates the tip based on the user input and format the tip amount
  * according to the local currency.
